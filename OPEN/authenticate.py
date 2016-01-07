43f188ef4c9f2f9b6e257b5b46751d07d91aa4ab
Justin Miron <justinmiron11@gmail.com>
2016-01-06 20:08:48 -0600

    cleaned python code

--- OPEN/authenticate.py
+++ OPEN/authenticate.py
@@ -1,28 +1,22 @@
+# Loads the authentication information and authorizes it with google
+#
+# All spreadsheets that are to be used must be shared with 
+#		open-service@pairing-gui.iam.gserviceaccount.com
 def LOAD(path_to_secret_file):
 	import json
 	import gspread
 	from oauth2client.client import SignedJwtAssertionCredentials
 
-	#this is loading your key given to you by THE google
+	# Load authentication information stored as a json file
 	json_key = json.load(open(path_to_secret_file))
-	#your not supposed to store this publicly,
-	#im not sure how to get around putting this...
-	#oh, ill just store it in the cloud somewhere,
-	#maybe
 
-	#This is the webpage that the api will find the sheets from. The
-	#sheets must be shared with the 'client_email' that is 
+	# website API finds the sheets from
 	scope = ['https://spreadsheets.google.com/feeds']
 
-	#gives you credientials to acess the url with the given key and your email
-	#since im using python2.7 i might not need to encode the key. yup.
+	# retrieve credentials from the scope, client_email, and private key
 	credentials = SignedJwtAssertionCredentials(json_key['client_email'], json_key['private_key'].encode(), scope)
 
-	#REMEMBER to share all needed sheets with whatever recorded
-	#as your 'client_email'
-	#SUCCESS!
-
-	#AUTHORIZE!
+	# authorize gspread using the credentials
 	gc = gspread.authorize(credentials)
 
 	return gc
